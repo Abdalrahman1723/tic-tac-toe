e0424c246a148abd83f82e27211f0d42cc3bc7ac
Abdalrahman Alaa Eldin <103855994+Abdalrahman1723@users.noreply.github.com>
2022-09-25 01:23:12 +0200

    edited-Abdalrahman

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -25,15 +25,16 @@ int main(void)
     else if (a == 1) // let's start
     {
         //-------------------------player one turn-------------------------------//
-        cout << "<<<<<<<<<<<player one start...>>>>>>>>>>>>>>\n";
         for (int i = 0; i < 5; i++)
         {
             check1 = true;
             check2 = true;
             while (check1)
             {
+                cout << "------------------------------------------------------------------------------------------------\n";
                 cout << thing << endl; //! print the shape
-                cout << "\t enter O position from 1 to 9 enter.\t 0 to withdraw\n";
+                cout << "\t\t\t<<<<<<<<<<<<<<player one turn>>>>>>>>>>>>>>>\n\n";
+                cout << "enter O position from 1 to 9 enter.\t 0 to withdraw\n";
                 cin >> x;
                 if (o[x] == 0 || x > 9 || x < 1)
                 {
@@ -112,6 +113,7 @@ int main(void)
             //--------------------------------------winner--------------------------------------//
             if (i >= 2)
             {
+                //-----------horizontal win--------------//
                 //! player one
                 if (thing[3] == 'O' && thing[7] == 'O' && thing[11] == 'O')
                 {
@@ -233,7 +235,129 @@ int main(void)
                         break;
                     }
                 }
-                //--------diagonal---------//
+                //---------vertical win-----------//
+                //! player one
+                if (thing[3] == 'O' && thing[31] == 'O' && thing[59] == 'O')
+                { // 1-4-7//
+                    cout << "player one wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<\n";
+                    ++count1;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player two
+                else if (thing[3] == 'X' && thing[31] == 'X' && thing[59] == 'X')
+                {
+                    cout << "player two wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<\n";
+                    ++count2;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+
+                //! player one
+                else if (thing[7] == 'O' && thing[35] == 'O' && thing[63] == 'O')
+                { // 2-5-8//
+                    cout << "player one wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count1;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player two
+                else if (thing[7] == 'X' && thing[35] == 'X' && thing[63] == 'X')
+                { // 2-5-8//
+                    cout << "player two wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count2;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player one
+                else if (thing[11] == 'O' && thing[39] == 'O' && thing[67] == 'O')
+                { // 3-6-9//
+                    cout << "player one wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count1;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player two
+                else if (thing[11] == 'X' && thing[39] == 'X' && thing[67] == 'X')
+                { // 3-6-9//
+                    cout << "player two wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count2;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //------------------------diagonal-------------------------//
                 //--------- 1 5 9 ---------//
                 //! player one
                 else if (thing[3] == 'O' && thing[35] == 'O' && thing[67] == 'O')
@@ -338,8 +462,10 @@ int main(void)
             //-------------------------player two turn-------------------------------//
             while (check2)
             {
+                cout << "------------------------------------------------------------------------------------------------\n";
                 cout << thing << endl; //! print the shape
-                cout << "enter X free position from 1 to 9\n";
+                cout << "\t\t\t<<<<<<<<<<<<<<player two turn>>>>>>>>>>>>>>>\n\n";
+                cout << "enter X free position from 1 to 9.\t 0 to withdraw\n";
                 cin >> x;
                 if (o[x] == 0 || x > 9 || x < 1)
                 {
@@ -417,6 +543,7 @@ int main(void)
             //--------------------------------------winner--------------------------------------//
             if (i >= 2)
             {
+                //---------------------horizontal win---------------------//
                 //! player one
                 if (thing[3] == 'O' && thing[7] == 'O' && thing[11] == 'O')
                 {
@@ -538,7 +665,129 @@ int main(void)
                         break;
                     }
                 }
-                //--------diagonal---------//
+                //---------vertical win-----------//
+                //! player one
+                if (thing[3] == 'O' && thing[31] == 'O' && thing[59] == 'O')
+                { // 1-4-7//
+                    cout << "player one wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<\n";
+                    ++count1;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player two
+                else if (thing[3] == 'X' && thing[31] == 'X' && thing[59] == 'X')
+                {
+                    cout << "player two wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<\n";
+                    ++count2;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+
+                //! player one
+                else if (thing[7] == 'O' && thing[35] == 'O' && thing[63] == 'O')
+                { // 2-5-8//
+                    cout << "player one wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count1;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player two
+                else if (thing[7] == 'X' && thing[35] == 'X' && thing[63] == 'X')
+                { // 2-5-8//
+                    cout << "player two wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count2;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player one
+                else if (thing[11] == 'O' && thing[39] == 'O' && thing[67] == 'O')
+                { // 3-6-9//
+                    cout << "player one wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count1;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //! player two
+                else if (thing[11] == 'X' && thing[39] == 'X' && thing[67] == 'X')
+                { // 3-6-9//
+                    cout << "player two wins..\n>>>>>>>>>>>>>|W|I|N|N|E|R|<<<<<<<<<<<<<<<<";
+                    ++count2;
+                    cout << "the score is \n player one----> " << count1 << endl;
+                    cout << "player two----> " << count2 << endl;
+                    cout << "do you want to play again?..\n 1-YES \n 2-NO \n"; //?play again question//
+                    cin >> y_or_n;
+                    if (y_or_n == 1)
+                    {
+                        main();
+                        return 0;
+                    }
+                    else
+                    {
+                        cout << "<<<<<<<<THANK YOU FOR PLAYING>>>>>>>>>\n";
+                        break;
+                    }
+                }
+                //---------------------diagonal win---------------------//
                 //--------- 1 5 9 ---------//
                 //! player one
                 else if (thing[3] == 'O' && thing[35] == 'O' && thing[67] == 'O')
@@ -645,7 +894,8 @@ int main(void)
         }
     } // the end
     else
-        cout<<"please enter (1) to start or (2) to quit\n try again \n";
+        cout << "please enter (1) to start or (2) to quit\n try again \n";
+
     return 0;
 }
 /*
